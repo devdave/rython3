c239ce3497c8d8cbb5168c24b333a230f6e85f9b
devdave <devdave@ominian.net>
2022-07-19 06:47:10 -0600

    code clean up

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 extern crate core;
 
 mod tokenizer;
-
 use std::{env};
-use std::io::{self, BufRead};
-use std::fs::File;
-use std::path::Path;
+
+
 
 use tokenizer::Processor;
 
-fn read_lines<P>(filename: P) -> Vec<String>
-where P: AsRef<Path>, {
-    let file = File::open(filename).unwrap();
-    io::BufReader::new(file).lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
-
-
-
-}
+// fn read_lines<P>(filename: P) -> Vec<String>
+// where P: AsRef<Path>, {
+//     let file = File::open(filename).unwrap();
+//     io::BufReader::new(file).lines()
+//         .map(|l| l.expect("Could not parse line"))
+//         .collect()
+//
+//
+//
+// }
 
 fn main() {
     println!("Hello, world!");
     let mut args = env::args();
     if args.len() == 2 {
+        let mut engine = Processor::consume_file(args.nth(1).expect("Expected a file name as argument"), Some("__name__".to_string()));
         println!("runtime.exe file_to_tokenize.py");
         // let lines = read_lines(args.nth(1).unwrap());
-        let result = Processor::Consume_file(args.nth(1).expect("Expected a file name as argument"));
+        let result = engine.run();
         // let result = Processor::Consume_lines(lines);
         if let Ok(retval) = result {
             for element in retval.iter() {
