49b56111598ac29ba113f726e461feb2fb7e4019
devdave <devdave@ominian.net>
2022-07-22 08:24:48 -0600

    Initial work on adjusting line start & end

--- src/tokenizer/token.rs
+++ src/tokenizer/token.rs
@@ -1,9 +1,10 @@
 use std::fmt::{Debug, Formatter};
 use crate::tokenizer::ttype::TType;
+use crate::tokenizer::position::Position;
 
 pub struct Token {
     pub r#type: TType,
-    pub lineno: usize,
+    pub line_start: usize,
     pub line_end: usize,
     pub col_start: usize,
     pub col_end: usize,
@@ -12,11 +13,11 @@ pub struct Token {
 
 #[allow(non_snake_case)]
 impl Token {
-    pub(crate) fn Make(ttype: TType, lineno: usize, col_start: usize, col_end: usize, content: &str) -> Self {
+    pub(crate) fn Make(ttype: TType, line_start: usize, col_start: usize, line_end: usize,  col_end: usize, content: &str) -> Self {
         Self {
             r#type: ttype,
-            lineno: lineno,
-            line_end: lineno,
+            line_start: line_start,
+            line_end: line_end,
             col_start: col_start,
             col_end: col_end,
             text: content.to_string(),
@@ -28,7 +29,7 @@ impl Debug for Token {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Token")
             .field("type", &self.r#type)
-            .field("lineno", &self.lineno)
+            .field("lineno", &self.line_start)
             .field("col_start", &self.col_start)
             .field("text", &self.text)
             .finish()
