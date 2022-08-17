51be5b2ee34f4a7366d9a6886a0c77a61a67830d
devdave <devdave@ominian.net>
2022-08-17 11:40:10 -0600

    Initial operators

---
+++ src/ast/op.rs
@@ -0,0 +1,37 @@
+use std::rc::Rc;
+
+use crate::tokenizer::Token;
+
+type TokenRef<'a> = Rc<Token<'a>>;
+
+pub struct AssignEqual<'a> {
+    pub(crate) tok: TokenRef<'a>,
+}
+
+pub enum UnaryOp {
+    Plus,
+    Minus,
+    BitInvert,
+    Not,
+}
+
+pub enum BinaryOperation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    FloorDivide,
+    Modulo,
+    Power,
+    LeftShift,
+    RightShift,
+    BitOr,
+    BitAnd,
+    BitXor,
+    MatrixMultiply,
+}
+
+pub enum BooleanOp {
+    And,
+    Or,
+}
