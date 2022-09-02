7777504e52a12a30cd41845f64ebad23111c1381
devdave <devdave@ominian.net>
2022-09-02 13:16:42 -0600

    switch over to use a &str instead of string

--- src/lexer/code_line.rs
+++ src/lexer/code_line.rs
@@ -4,15 +4,15 @@ use unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 
 
-#[derive(Clone)]
-pub struct CodeLine {
-    line: String,
+#[derive(Clone, Copy)]
+pub struct CodeLine<'a> {
+    line: &'a str,
     len: usize,
     pos: usize,
 }
 
-impl CodeLine {
-    pub fn new(input: String) -> Self {
+impl <'a> CodeLine<'a> {
+    pub fn new(input: &'a str) -> Self {
         Self {
             len: input.len(),
             line: input,
@@ -43,26 +43,37 @@ impl CodeLine {
         self.len.saturating_sub(self.pos)
     }
 
-    pub fn get_line(&self) -> String {
+    pub fn get_line(&self) -> &str {
         self.line.clone()
+    }
+
+    pub fn peek(&self) -> Option<&str> {
+        return self.line.graphemes(true).nth(self.pos);
+    }
+
+    pub fn get(&mut self) -> Option<&str> {
+        let retval = self.line.graphemes(true).nth(self.pos);
+        self.pos = self.pos + 1;
+        return retval;
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::ast::CompoundStatement::ClassDef;
+
+
     use super::*;
 
     #[test]
     fn basic() {
-        let line = CodeLine::new("Hello World\n".to_string());
+        let line = CodeLine::new("Hello World\n");
 
         assert_eq!(line.remaining(), 12);
     }
 
     #[test]
     fn collect_numbers() {
-        let mut line = CodeLine::new("12345abc\n".to_string());
+        let mut line = CodeLine::new("12345abc\n");
         let re = Regex::new(r"\A\d+").expect("regex");
         let outcome = line.return_match(re);
         assert!(outcome != None);
@@ -70,8 +81,30 @@ mod test {
         if let Some((new_pos, retval)) = outcome {
             assert_eq!(new_pos, 5);
             assert_eq!(retval, "12345");
-
+        } else {
+            panic!("Failed to match numbers!");
         }
+
+    }
+
+    #[test]
+    fn collect_numbers_and_then_peek() {
+        let mut line = CodeLine::new("12345abc\n");
+        let re = Regex::new(r"\A\d+").expect("regex");
+        let outcome = line.return_match(re);
+        assert!(outcome != None);
+
+        assert_eq!(outcome, Some((5 as usize, "12345".to_string())));
+
+        assert_eq!(line.peek(), Some("a"));
+        assert_eq!(line.get(), Some("a"));
+        assert_eq!(line.get(), Some("b"));
+        assert_eq!(line.get(), Some("c"));
+        assert_eq!(line.get(), Some("\n"));
+        assert_eq!(line.get(), None);
+        assert_eq!(line.remaining(), 0);
+
+
 
 
     }
